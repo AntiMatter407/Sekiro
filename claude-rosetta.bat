8cc2e342bbf8a733714a9e14c21b0c4f428aba41
GuoYanping <1922350825@qq.com>
2026-06-18 10:30:56 +0800

    清理

--- claude-rosetta.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set NODE_EXTRA_CA_CERTS=C:\Users\guoya\AppData\Local\Temp\rosetta-ca-cert.pem
-set NODE_TLS_REJECT_UNAUTHORIZED=0
-"C:\Users\guoya\AppData\Local\Microsoft\WinGet\Packages\Anthropic.ClaudeCode_Microsoft.Winget.Source_8wekyb3d8bbwe\claude.exe" %*
